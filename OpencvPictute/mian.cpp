f2b123b2bc1419c22b28de92e4e1b905d192fdd1
MasterBoot <15771335697@163.com>
2015-11-29 15:16:14 +0800

    人脸识别程序环境移植测试
    环境移植VS2010到VS2013;win7_x86到win10_x64.

--- OpencvPictute/mian.cpp
+++ OpencvPictute/mian.cpp
@@ -1,62 +1,108 @@
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+#include "cv.h" 
+#include "highgui.h"
 
-using namespace cv;
-using namespace std;
-Mat src; Mat src_gray;
-int thresh = 100;
-int max_thresh = 255;
-RNG rng(12345);
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <assert.h> 
+#include <math.h> 
+#include <float.h> 
+#include <limits.h> 
+#include <time.h> 
+#include <ctype.h>
 
-/// Function header
-void thresh_callback(int, void*);
+#ifdef _EiC 
+#define WIN32 
+#endif
 
-/** @function main */
+static CvMemStorage* storage = 0;
+static CvHaarClassifierCascade* cascade = 0;
+
+void detect_and_draw(IplImage* image);
+
+const char* cascade_name =
+"haarcascade_frontalface_alt.xml";
+/*    "haarcascade_profileface.xml";*/
+
 int main(int argc, char** argv)
 {
-	/// 加载源图像
-	src = imread("06.jpg");
+	cascade_name = "haarcascade_frontalface_alt2.xml";
+	cascade = (CvHaarClassifierCascade*)cvLoad(cascade_name, 0, 0, 0);
 
-	/// 转成灰度并模糊化降噪
-	cvtColor(src, src_gray, CV_BGR2GRAY);
-	blur(src_gray, src_gray, Size(3, 3));
-
-	/// 创建窗体
-	char* source_window = "Source";
-	namedWindow(source_window, CV_WINDOW_AUTOSIZE);
-	imshow(source_window, src);
-
-	createTrackbar(" Canny thresh:", "Source", &thresh, max_thresh, thresh_callback);
-	thresh_callback(0, 0);
-
-	waitKey(0);
-	return(0);
-}
-
-/** @function thresh_callback */
-void thresh_callback(int, void*)
-{
-	Mat canny_output;
-	vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
-
-	/// 用Canny算子检测边缘
-	Canny(src_gray, canny_output, thresh, thresh * 2, 3);
-	/// 寻找轮廓
-	findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-
-	/// 绘出轮廓
-	Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
-	for (int i = 0; i< contours.size(); i++)
+	if (!cascade)
 	{
-		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-		drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+		fprintf(stderr, "ERROR: Could not load classifier cascade\n");
+		return -1;
+	}
+	storage = cvCreateMemStorage(0);
+	cvNamedWindow("result", 1);
+
+	//const char* filename = "01.jpg";
+	IplImage* image = cvLoadImage("01.jpg", 1);
+
+	if (image)
+	{
+		detect_and_draw(image);
+		cvWaitKey(0);
+		cvReleaseImage(&image);
 	}
 
-	/// 在窗体中显示结果
-	namedWindow("Contours", CV_WINDOW_AUTOSIZE);
-	imshow("Contours", drawing);
+	cvDestroyWindow("result");
+
+	return 0;
+}
+
+
+void detect_and_draw(IplImage* img)
+{
+	double scale = 1.2;
+	static CvScalar colors[] = {
+		{ 0, 0, 255 } , { 0, 128, 255 }, { 0, 255, 255 }, { 0, 255, 0 },
+		{ 255, 128, 0 }, { 255, 255, 0 }, { 255, 0, 0 }, { 255, 0, 255 }
+	};//Just some pretty colors to draw with
+
+	//Image Preparation 
+	// 
+	IplImage* gray = cvCreateImage(cvSize(img->width, img->height), 8, 1);
+	IplImage* small_img = cvCreateImage(cvSize(cvRound(img->width / scale), cvRound(img->height / scale)), 8, 1);
+	cvCvtColor(img, gray, CV_BGR2GRAY);
+	cvResize(gray, small_img, CV_INTER_LINEAR);
+
+	cvEqualizeHist(small_img, small_img); //直方图均衡
+
+	//Detect objects if any 
+	// 
+	cvClearMemStorage(storage);
+	double t = (double)cvGetTickCount();
+	CvSeq* objects = cvHaarDetectObjects(small_img,
+		cascade,
+		storage,
+		1.1,
+		2,
+		0/*CV_HAAR_DO_CANNY_PRUNING*/,
+		cvSize(30, 30));
+
+	t = (double)cvGetTickCount() - t;
+	printf("detection time = %gms\n", t / ((double)cvGetTickFrequency()*1000.));
+
+	//Loop through found objects and draw boxes around them 
+	for (int i = 0; i<(objects ? objects->total : 0); ++i)
+	{
+		CvRect* r = (CvRect*)cvGetSeqElem(objects, i);
+		cvRectangle(img, cvPoint(r->x*scale, r->y*scale), cvPoint((r->x + r->width)*scale, (r->y + r->height)*scale), colors[i % 8]);
+	}
+	for (int i = 0; i < (objects ? objects->total : 0); i++)
+	{
+		CvRect* r = (CvRect*)cvGetSeqElem(objects, i);
+		CvPoint center;
+		int radius;
+		center.x = cvRound((r->x + r->width*0.5)*scale);
+		center.y = cvRound((r->y + r->height*0.5)*scale);
+		radius = cvRound((r->width + r->height)*0.25*scale);
+		cvCircle(img, center, radius, colors[i % 8], 3, 8, 0);
+	}
+
+	cvShowImage("result", img);
+	cvReleaseImage(&gray);
+	cvReleaseImage(&small_img);
 }
