286113e091a2123469365d8e17f4346daa3ae7fc
MasterBoot <15771335697@163.com>
2015-12-04 09:43:37 +0800

    新增-人脸数量计数
    利用循环变量,标记的次数,对人脸个数进行统计,完毕后在控制台用piuntf();输出。

--- OpencvPictute/mian.cpp
+++ OpencvPictute/mian.cpp
@@ -1,6 +1,5 @@
 #include "cv.h" 
 #include "highgui.h"
-
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h> 
@@ -10,7 +9,9 @@
 #include <limits.h> 
 #include <time.h> 
 #include <ctype.h>
-
+#include<opencv2\core\core.hpp>
+#include<opencv2\imgproc\imgproc.hpp>
+#include<opencv2\highgui\highgui.hpp>
 #ifdef _EiC 
 #define WIN32 
 #endif
@@ -34,9 +35,9 @@ int main( int argc, char** argv )
         return -1; 
     } 
     storage = cvCreateMemStorage(0); 
-    cvNamedWindow( "result", 1 ); 
+    cvNamedWindow( "窗口", 1 ); 
      
-    const char* filename = "last2.jpg"; 
+    const char* filename = "face.jpg"; 
     IplImage* image = cvLoadImage( filename, 1 );
     if( image ) 
     { 
@@ -45,7 +46,7 @@ int main( int argc, char** argv )
         cvReleaseImage( &image );   
     }
 
-    cvDestroyWindow("result"); 
+    cvDestroyWindow("窗口"); 
   
     return 0; 
 }
@@ -54,8 +55,8 @@ void detect_and_draw(IplImage* img )
 { 
     double scale=0.8; //缩放因子，图片缩小检测提高检测速度
     static CvScalar colors[] = { 
-        {{0,0,255}},{{0,128,255}},{{0,255,255}},{{0,255,0}}, 
-        {{255,128,0}},{{255,255,0}},{{255,0,0}},{{255,0,255}} 
+        {0,0,255},{0,128,255},{0,255,255},{0,255,0}, 
+        {255,128,0},{255,255,0},{255,0,0},{255,0,255} 
     };//Just some pretty colors to draw with
 
     //Image Preparation 
@@ -71,24 +72,19 @@ void detect_and_draw(IplImage* img )
     // 
     cvClearMemStorage(storage); 
     double t = (double)cvGetTickCount(); 
-    CvSeq* objects = cvHaarDetectObjects(small_img, 
-                                                                        cascade, 
-                                                                        storage, 
-                                                                        1.1, 
-                                                                        2, 
-                                                                        0/*CV_HAAR_DO_CANNY_PRUNING*/, 
-                                                                        cvSize(30,30));
+    CvSeq* objects = cvHaarDetectObjects(small_img, cascade,storage,1.1,2,0,cvSize(30,30));
 
     t = (double)cvGetTickCount() - t; 
     printf( "detection time = %gms\n", t/((double)cvGetTickFrequency()*1000) );
 
     //Loop through found objects and draw boxes around them 
+	int faceCount;
     for(int i=0;i<(objects? objects->total:0);++i) 
     { 
         CvRect* r=(CvRect*)cvGetSeqElem(objects,i); 
         cvRectangle(img, cvPoint(r->x*scale,r->y*scale), cvPoint((r->x+r->width)*scale,(r->y+r->height)*scale), colors[i%8]); 
     } 
-    for( int i = 0; i < (objects? objects->total : 0); i++ ) 
+    for( int i = 0; i < (objects? objects->total : 0); i++ ) //i为检测到人脸的数量
     { 
         CvRect* r = (CvRect*)cvGetSeqElem( objects, i ); 
         CvPoint center; 
@@ -98,9 +94,11 @@ void detect_and_draw(IplImage* img )
         radius = cvRound((r->width + r->height)*0.25*scale); //获取半径
  //对img脸部进行圆圈标注。参数为：图像，圆心坐标，圆心半径，线条颜色，线条粗细负数表示填充元，线条类型，圆心坐标点和半径值的小数点位数
         cvCircle( img, center, radius, colors[i%8], 1, 8, 0 );
+		faceCount = i;
     }
 
-    cvShowImage( "result", img ); //显示结果窗口
+	printf("已经检测到的人脸个数为：%d个\n", faceCount);
+    cvShowImage( "窗口", img ); //显示结果窗口
     cvReleaseImage(&gray); //显示图像
     cvReleaseImage(&small_img); 
 }
