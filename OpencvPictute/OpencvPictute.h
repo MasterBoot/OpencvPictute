40decb895793d7a16cb51e40aec86efb257bf05c
MasterBoot <15771335697@163.com>
2015-12-09 17:18:35 +0800

    控制台程序迁移到MFC基于对话框

---
+++ OpencvPictute/OpencvPictute.h
@@ -0,0 +1,32 @@
+
+// OpencvPictute.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// COpencvPictuteApp:
+// 有关此类的实现，请参阅 OpencvPictute.cpp
+//
+
+class COpencvPictuteApp : public CWinApp
+{
+public:
+	COpencvPictuteApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern COpencvPictuteApp theApp;
--- OpencvPictute/OpencvPictuteDlg.cpp
+++ OpencvPictute/OpencvPictuteDlg.cpp
@@ -1,3 +1,11 @@
+
+// OpencvPictuteDlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "OpencvPictute.h"
+#include "OpencvPictuteDlg.h"
+#include "afxdialogex.h"
 #include "cv.h" 
 #include "highgui.h"
 #include <stdio.h> 
@@ -12,9 +20,85 @@
 #include<opencv2\core\core.hpp>
 #include<opencv2\imgproc\imgproc.hpp>
 #include<opencv2\highgui\highgui.hpp>
-#ifdef _EiC 
-#define WIN32 
+#ifdef _DEBUG
+#define new DEBUG_NEW
 #endif
+
+
+// COpencvPictuteDlg 对话框
+
+
+
+
+COpencvPictuteDlg::COpencvPictuteDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(COpencvPictuteDlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void COpencvPictuteDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(COpencvPictuteDlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, &COpencvPictuteDlg::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// COpencvPictuteDlg 消息处理程序
+
+BOOL COpencvPictuteDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 设置此对话框的图标。当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void COpencvPictuteDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR COpencvPictuteDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
 
 static CvMemStorage* storage = 0; 
 static CvHaarClassifierCascade* cascade = 0;
@@ -23,16 +107,16 @@ void detect_and_draw( IplImage* image );
 
 const char* cascade_name = 
 "haarcascade_frontalface_alt.xml"; 
-/*    "haarcascade_profileface.xml";*/
 
-int main( int argc, char** argv ) 
-{ 
-    cascade_name = "haarcascade_frontalface_alt2.xml"; 
+
+void COpencvPictuteDlg::OnBnClickedButton1()
+{
+	cascade_name = "haarcascade_frontalface_alt2.xml"; 
     cascade = (CvHaarClassifierCascade*)cvLoad( cascade_name, 0, 0, 0 ); 
     if( !cascade ) 
     { 
         fprintf( stderr, "ERROR: Could not load classifier cascade\n" ); 
-        return -1; 
+       // return -1; 
     } 
     storage = cvCreateMemStorage(0); 
     cvNamedWindow( "窗口", 1 ); 
@@ -48,8 +132,6 @@ int main( int argc, char** argv )
     }
 
     cvDestroyWindow("窗口"); 
-  
-    return 0; 
 }
 
 void detect_and_draw(IplImage* img ) 
@@ -57,8 +139,8 @@ void detect_and_draw(IplImage* img )
     double scale=0.8; //缩放因子，图片缩小检测提高检测速度
 	/*face.jpg共有75张人脸*/
     static CvScalar colors[] = { 
-        {0,0,255},{0,128,255},{0,255,255},{0,255,0}, 
-        {255,128,0},{255,255,0},{255,0,0},{255,0,255} 
+        (0,0,255),(0,128,255),(0,255,255),(0,255,0), 
+        (255,128,0),(255,255,0),(255,0,0),(255,0,255) 
     };//Just some pretty colors to draw with
 
     //Image Preparation 
